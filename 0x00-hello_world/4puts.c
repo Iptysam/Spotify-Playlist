2397f1b3ddbcc433f16b40bb3049943334a48ed0
Iptysam <mukadamiptisam@gmail.com>
2022-04-09 07:53:50 -0700

    a c program that prints a string

---
+++ 0x00-hello_world/4puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* puts -print declared char string to std output
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
