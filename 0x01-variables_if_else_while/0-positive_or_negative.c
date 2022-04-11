4b11c4b83c389779d05c13e1a1e13abe81ed4a5b
Iptysam <mukadamiptisam@gmail.com>
2022-04-11 10:37:00 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,6 +5,7 @@
 * main - Entry point
 * if...else if...else statement to determine value of n
 * Return: Always 0 (Success)
+**/
 /* betty style doc for function main goes there */
 int main(void)
 {
