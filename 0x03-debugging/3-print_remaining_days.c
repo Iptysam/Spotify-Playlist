acbf165e0c43f7f305ee202ba98a824aca8be798
Iptysam <mukadamiptisam@gmail.com>
2022-04-20 14:19:48 -0700

    remaining days in  a year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -26,7 +26,7 @@ else
 {
 if (month == 2 && day == 60)
 {
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year)
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
 {
