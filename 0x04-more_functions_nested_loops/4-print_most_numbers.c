97f73d9db058d2fb36f049b96ad7bda24ece7ad5
Iptysam <mukadamiptisam@gmail.com>
2022-04-21 04:54:43 -0700

    to print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,15 +9,15 @@
 
 void print_most_numbers(void)
 {
-int i = 0;
+int i;
 
-while (i < 10)
+for (i = 0; i < 10; i++)
 {
-if ((i != 2) && (i != 4))
+if ((i == 2) || (i == 4))
 {
-_putchar('0' + i);
-i++;
+continue;
 }
+_putchar('0' + i);
 }
 _putchar('\n');
 }
