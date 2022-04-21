9b0762345c39c3bd312ea7087bb2f6d1e915bdab
Iptysam <mukadamiptisam@gmail.com>
2022-04-21 10:15:39 -0700

    to print numbers from 1 to 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,19 +8,23 @@
 int main(void)
 {
 int i;
-for (i = 0; i <= 100; i++)
+
+for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0)
 {
+if (i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else
+{
 printf("Fizz");
+}
 }
 else if (i % 5 == 0)
 {
 printf("Buzz");
-}
-else if (i % 3 == 0 && i % 5 == 0)
-{
-printf("FizzBuzz");
 }
 else
 {
@@ -29,6 +33,6 @@ printf("%d", i);
 if (i < 100)
 putchar(' ');
 }
-_putchar("\n");
+printf("\n");
 return (0);
 }
