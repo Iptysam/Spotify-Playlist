cc602d679735693661d15cb17beffb9a57ed4c60
Iptysam <mukadamiptisam@gmail.com>
2022-04-21 05:31:09 -0700

    to print a diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal
+ * @n: first operand
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('\');
+}
+_putchar('\n');
+}
