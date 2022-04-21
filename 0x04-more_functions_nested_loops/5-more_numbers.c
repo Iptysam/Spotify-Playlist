e39c3f91cf7695fbf69f143c6b35aa5570a3132a
Iptysam <mukadamiptisam@gmail.com>
2022-04-21 05:09:24 -0700

    to print most numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * more_numbers - to print numbers from 0 to 14 
+ * 10 times
+ * Return: void
  */
 
 void more_numbers(void)
 {
-int i,j;
+int i, j;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 15; j++)
