fcb6f100ed870fc9540b4bd6e114ecae9425108f
Iptysam <mukadamiptisam@gmail.com>
2022-04-22 02:14:20 -0700

    prints sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of number
+ * @n: first operand
+ * Return: 1 when n > 0, 0 when n = 0,
+ * -1 when n < 0
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+}
+if (n < 0)
+{
+_putchar('-');
+}
+else
+{
+_putchar('0');
+}
+_putchar('\n');
+}
