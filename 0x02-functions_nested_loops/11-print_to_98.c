bdc3cd405ed675217a9ba2f25ef731e29379b4c2
Iptysam <mukadamiptisam@gmail.com>
2022-04-22 07:42:02 -0700

    adds two integers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print from 0 to 98
+ * @n: first operand
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+int i, j;
+
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+if (i != 98)
+printf("%d, ", i);
+else if (i == 98)
+printf("%d\n", i);
+}
+} else if (n >= 98)
+{
+for (j = n; j >= 98; j--)
+{
+if (j != 98)
+printf("%d, ", j);
+else if (j == 98)
+printf("%d\n", j);
+}
+}
+}
