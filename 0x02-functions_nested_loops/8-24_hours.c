2138e98ad85aa345ec39b5f88cafed8fb6e757ec
Iptysam <mukadamiptisam@gmail.com>
2022-04-22 06:03:07 -0700

    prints time of jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,9 +16,9 @@ j = 0;
 while (j < 60)
 {
 _putchar((i / 10) + '0');
-_putchar((j % 10) + '0');
+_putchar((i % 10) + '0');
 _putchar(':');
-_putchar((i / 10) + '0');
+_putchar((j / 10) + '0');
 _putchar((j % 10) + '0');
 _putchar('\n');
 j++;
