ec9e9e52a398d6b9e709ee4d2953def6496c2358
Iptysam <mukadamiptisam@gmail.com>
2022-04-22 05:20:06 -0700

    prints sign of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of number
+ * @n: first operand
+ * Return: value of last digit
+ */
+
+int print_last_digit(int n)
+{
+int d;
+d = n % 10;
+return (d);
+}
