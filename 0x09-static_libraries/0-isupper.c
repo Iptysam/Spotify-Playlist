936c843c78125a7b32c625642f2362bf5ffe3aae
Iptysam <89805073+Iptysam@users.noreply.github.com>
2022-05-06 17:59:56 +0300

    uppercase

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
