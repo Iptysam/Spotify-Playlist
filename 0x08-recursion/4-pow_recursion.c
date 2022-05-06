459fb99719de0ccebca166c8b838491e59518737
Iptysam <mukadamiptisam@gmail.com>
2022-05-06 05:52:48 -0700

    x power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - value of x raised to the power of y
+ * @x: first integer
+ * @y: second integer
+ * Return: on  error -1, success 0
+ */
+
+int _pow_recursion(int x,int y)
+{
+if (y < 0)
+return (-1);
+return (x ** y);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,4 @@
 void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
